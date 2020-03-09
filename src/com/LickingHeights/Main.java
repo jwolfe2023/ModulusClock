ee0f6129d628cb602fa797cff204a4f93261957d
James Wolfe <james.wolfe.2023@student.lhschools.org>
2020-03-09 12:06:46 -0400

    UserDataCaptured

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,38 @@
 package com.LickingHeights;
 
+import javax.swing.plaf.synth.SynthEditorPaneUI;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	modulusClock();
+
+
+
+    }
+
+    public static void modulusClock() {
+
+        int hours;
+
+        int minutes;
+
+        Scanner keyboard;
+
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Give Me An Hour: ");
+
+        hours = keyboard.nextInt();
+
+        System.out.println("Give Me A Amount of Minute(s): ");
+
+        minutes = keyboard.nextInt();
+
+        System.out.println(hours);
+
+        System.out.println(minutes);
+
     }
 }
